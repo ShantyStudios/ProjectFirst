51024908bcd9823b737c6480280caa00c8f08a69
Deliquescence <misterbeez01@gmail.com>
2014-06-08 22:17:42 -0400

    game
    uploaded and ready to use

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = MyGdxGame.Title ;
+		config.width =  MyGdxGame.V_Width*MyGdxGame.Scale;
+		config.height =  MyGdxGame.V_Height*MyGdxGame.Scale;
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
